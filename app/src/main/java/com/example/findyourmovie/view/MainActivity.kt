0c8988e0ea3fec69e8f0a9d5722dba4c8087d347
LARAVISH <73416118+LARAVISH@users.noreply.github.com>
2022-05-19 13:56:00 +0500

    added bottom navigation menu

--- app/src/main/java/com/example/findyourmovie/view/MainActivity.kt
+++ app/src/main/java/com/example/findyourmovie/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.findyourmovie
+package com.example.findyourmovie.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.findyourmovie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
